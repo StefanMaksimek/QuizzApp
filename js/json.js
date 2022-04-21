9b8e8b4eb8b66fdaeedbde5e477d85065d1fcc85
Stefan Maksimek <stefan.maksimek@gmail.com>
2022-04-21 19:52:04 +0200

    debugg style img

--- js/json.js
+++ js/json.js
@@ -3,7 +3,7 @@ let questions = [
         "frage": "Frage 1",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/poker.png"
     },
     {
         "frage": "Frage 2",
@@ -15,49 +15,49 @@ let questions = [
         "frage": "Frage 3",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/cardheader.png"
+        "img": "./img/fallschirm.jpg"
     },
     {
         "frage": "Frage 4",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/taucher.jpg"
     },
     {
         "frage": "Frage 5",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/snowboard.jpg"
     },
     {
         "frage": "Frage 6",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/code.jpg"
     },
     {
         "frage": "Frage 7",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/dachstuhl.jpg"
     },
     {
         "frage": "Frage 8",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/sniper.jpg"
     },
     {
         "frage": "Frage 9",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/hochzeitsringe.jpg"
     },
     {
         "frage": "Frage 10",
         "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
         "lösung1": "2",
-        "img": "./img/question-shield-64.png"
+        "img": "./img/formel1.jpg"
     }
 ];
 let answers = [];
