983b727e0e6330b0643c1a089965e992cfbe72cf
Stefan Maksimek <stefan.maksimek@gmail.com>
2022-04-24 10:58:43 +0200

    update questions

--- js/json.js
+++ js/json.js
@@ -1,62 +1,62 @@
 let questions = [
     {
-        "frage": "Frage 1",
-        "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
-        "lösung1": 2,
+        "frage": "Welches ist die beste Pokerhand?",
+        "answeres": ["4 Asse", "Royal Flush", "5 Karten einer Farbe", "4 Karten + JOKER"],
+        "lösung1": 1,
         "img": "./img/poker.png"
     },
    {
-       "frage": "Frage 2",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
+       "frage": "Worauf kommt es beim Trading an?",
+       "answeres": ["Kaffesatz lesen", "Wilkürliche Informationen aus der Vergangenheit", "Volumen", "Winkel vom Mond zur Sonne"],
        "lösung1": 2,
        "img": "./img/trading.jpg"
    },
    {
-       "frage": "Frage 3",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
-       "lösung1": 2,
+       "frage": "Aus welcher Höhe werden Deutsche Fallschirmjäger mit der Rundkappe abgsetzt?",
+       "answeres": ["ca.400m", "Flügellänge des Flugzeugs * anzahl der Springer", "abhängig von Wind und Uhrzeit", "Dienstzeit * 10"],
+       "lösung1": 0,
        "img": "./img/fallschirm.jpg"
    },
    {
-       "frage": "Frage 4",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
-       "lösung1": 2,
+       "frage": "In welchem Element befindet sich diese Person?",
+       "answeres": ["Feuer", "Wasser", "Erde", "Luft"],
+       "lösung1": 1,
        "img": "./img/taucher.jpg"
    },
    {
-       "frage": "Frage 5",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
+       "frage": "Welches Fortbewegungsmittel wird hier verwendet",
+       "answeres": ["Schlitten", "Auto", "Ski", "Snowboard"],
        "lösung1": 2,
        "img": "./img/snowboard.jpg"
    },
    {
-       "frage": "Frage 6",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
+       "frage": "Welchen Code kannst du hier sehen",
+       "answeres": ["HTMi", "HTLM", "HTML", "HLMT"],
        "lösung1": 2,
        "img": "./img/code.jpg"
    },
    {
-       "frage": "Frage 7",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
-       "lösung1": 2,
+       "frage": "Was für ein Stuhl ist hier zu sehen",
+       "answeres": ["Dachstuhl", "TV-Stuhl", "Drehstuhl", "Gar kein Stuhl"],
+       "lösung1": 0,
        "img": "./img/dachstuhl.jpg"
    },
    {
-       "frage": "Frage 8",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
-       "lösung1": 2,
+       "frage": "Was kannst du sehen?",
+       "answeres": ["nix", "gar nix", "mehr als überhaupt nix", "Sniper"],
+       "lösung1": 3,
        "img": "./img/sniper.jpg"
    },
    {
-       "frage": "Frage 9",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
-       "lösung1": 2,
+       "frage": "Aus welchem Material sind diese Ringe",
+       "answeres": ["Gold", "Plastik", "Uran", "Käse"],
+       "lösung1": 0,
        "img": "./img/hochzeitsringe.jpg"
    },
    {
-       "frage": "Frage 10",
-       "answeres": ["antwort1antwort1", "antwort2antwort2", "antwort3antwort3", "antwort4antwort4"],
-       "lösung1": 2,
+       "frage": "Welche Teams sind Hier zusehen",
+       "answeres": ["Mercedes / Ferrari", "Mercedes / Renault", "Mercedes / RedBull", "Renault / Ferrari"],
+       "lösung1": 0,
        "img": "./img/formel1.jpg"
    }
 ];
